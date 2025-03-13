39aa5906e39a3c6eca89b77e6fddf2b699487ff5
kyakyak <udosjdjdjdj@gmail.com>
2025-03-13 10:23:04 +0900

    main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main():
     
     path = 'best_SSD_model.pth'
     
-    # train_model(train_loader, val_loader, path=path)
-    # run_evaluation(val_loader, path=path)
+    train_model(train_loader, val_loader, path=path)
+    run_evaluation(val_loader, path=path)
     model_test(test_dataset, path=path)
 
 if __name__ == '__main__':
